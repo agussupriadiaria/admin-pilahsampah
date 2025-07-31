9e11167f38fcb029abc0b7fbd554fddbfd7c7a14
agus.supriadi.aria <agus.supriadi.aria@gmail.com>
2025-07-31 13:49:57 +0000

    update

--- pages/User Data/jsobjects/reloadPage/reloadPage.js
+++ pages/User Data/jsobjects/reloadPage/reloadPage.js
@@ -2,7 +2,7 @@ export default {
 	myVar1: [],
 	myVar2: {},
 	myFun1 () {
-		navigateTo('User Data', {}, 'SAME_WINDOW');
+		navigateTo('Transaction Processing', {}, 'SAME_WINDOW');
 		//	write code here
 		//	this.myVar1 = [1,2,3]
 	},
